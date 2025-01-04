f1f54917018946b178610717a4014e6d5e41b852
DaradMosa <118133163+DaradMosa@users.noreply.github.com>
2025-01-04 20:29:03 +0300

    start on open ai

--- app/src/main/java/com/example/myapplication/OpenAIService.java
+++ app/src/main/java/com/example/myapplication/OpenAIService.java
@@ -9,7 +9,7 @@ import retrofit2.http.POST;
 //HTTP CALL
 public interface OpenAIService{
     @Headers({
-            "Authorization: Bearer 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", // Replace with your OpenAI API key
+            "Authorization: Bearer ", // Replace with your OpenAI API key
             "Content-Type: application/json"
     })
     @POST("v1/chat/completions")
